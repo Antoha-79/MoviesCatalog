cf68a88c43e4433998f263bcc45295059583de8c
Anton Dovnar <adam_s@mail.ru>
2022-03-17 14:42:16 +0300

    картинку исправил как подтягивает на странице категорий фильмов

--- Proect-MovieCatalog/Modules/ListOfMovies/listOfMoviesCollectVCell.swift
+++ Proect-MovieCatalog/Modules/ListOfMovies/listOfMoviesCollectVCell.swift
@@ -10,12 +10,16 @@ import UIKit
 class listOfMoviesCollectVCell: UICollectionViewCell {
     
     
-    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var imageView: DownloadPoster!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
 
 func setup(movie: MovieMDB) {
-    imageView.image = UIImage(named: "TestImage") // movie.image // ВРЕМЕННО, пока нет сохраненных
+    //imageView.image = UIImage(named: "TestImage") // movie.image // ВРЕМЕННО, пока нет сохраненных
+    imageView.load(imageURLStr: movie.poster_path) { image in
+        self.imageView?.image = image
+    }
+    
     titleLabel.text = movie.title
     descriptionLabel.text = "ПОКА ТЕСТ" // movie.genre[0].rawValue//  ВРЕМЕННО
     
