d5bf5226198e8156174d2d382805f795a0d0a4b0
Anton Dovnar <adam_s@mail.ru>
2022-03-18 10:54:56 +0300

    Текст в лейбу с рейтингом Хорошо бы переделать страницу фильма - мало инфы и не красиво

--- Proect-MovieCatalog/Modules/TheMovieVC/RatingTablViwCell.swift
+++ Proect-MovieCatalog/Modules/TheMovieVC/RatingTablViwCell.swift
@@ -13,8 +13,8 @@ class RatingTablViwCell: UITableViewCell {
     @IBOutlet private weak var imdbCount: UILabel!
 
     func setup(movie: MovieMDB) {
-        imdbRating.text = "\(movie.vote_average)"
-        imdbCount.text = "\(movie.vote_count)"
+        imdbRating.text = "Rating IMDB: \(movie.vote_average)"
+        imdbCount.text = "Проголосовало: \(movie.vote_count)"
     }
 
     override func awakeFromNib() {
